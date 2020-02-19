c204a11c1f0607a8d876b943b8bc210308dafd49
Frank Rügamer <frankruegamer@gmail.com>
2020-02-19 20:16:51 +0100

    Initial commit

---
+++ src/main/java/com/frankruegamer/springdocauthenticationbug/SpringdocAuthenticationBugApplication.java
@@ -0,0 +1,13 @@
+package com.frankruegamer.springdocauthenticationbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocAuthenticationBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdocAuthenticationBugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frankruegamer/springdocauthenticationbug/SpringdocAuthenticationBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankruegamer.springdocauthenticationbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocAuthenticationBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
